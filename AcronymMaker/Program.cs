92782cb6b5814a768aba68b7e0cd44c1f4c788f4
Samantha Davis <sami.davis98@outlook.com>
2020-11-03 16:13:43 -0600

    Created AcronymMaker project

---
+++ AcronymMaker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AcronymMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a multi-word term");
+            string input=Console.ReadLine();
+          input=input.ToUpper();
+          string[] words=input.Split();
+          foreach(string s in words)
+          {
+            Console.Write(s[0]);
+          }
+        }
+
+    }
+}
