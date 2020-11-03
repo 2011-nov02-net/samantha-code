49f2eea8e861be6c29e43de9c4f07ed819c08b8b
Samantha Davis <sami.davis98@outlook.com>
2020-11-03 16:32:33 -0600

    creating and using ProceduralBasics files

---
+++ ProceduralBasics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProceduralBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a multi-word term");
+         
+        }
+    }
+}
